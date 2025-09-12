7dbeed998eeb5a096fbc8039bfc24b0116e28219
Anastasia <a.lilova@sertink.ru>
2025-09-12 11:31:32 +0300

    rewiew1 test1

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,139 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
+import { useRef, useState } from 'react';
+import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
+import {
+	ArticleStateType,
+	defaultArticleState,
+	backgroundColors,
+	fontSizeOptions,
+	fontColors,
+	contentWidthArr,
+	fontFamilyOptions,
+} from 'src/constants/articleProps';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	articleState: ArticleStateType;
+	setArticleState: (props: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	articleState,
+	setArticleState,
+}: ArticleParamsFormProps) => {
+	const [isSidebarOpen, setSidebarIsOpen] = useState(false);
+
+	const [fontFamily, setFontFamily] = useState(articleState.fontFamilyOption);
+	const [fontSize, setFontSize] = useState(articleState.fontSizeOption);
+	const [fontColor, setFontColor] = useState(articleState.fontColor);
+	const [backgroundColor, setBackgroundColor] = useState(
+		articleState.backgroundColor
+	);
+	const [contentWidth, setContentWidth] = useState(articleState.contentWidth);
+
+	const sidebarRef = useRef<HTMLDivElement | null>(null);
+
+	// Закрыть при клике вне сайдбара
+
+	useOutsideClickClose({
+		isOpen: isSidebarOpen,
+		rootRef: sidebarRef,
+		onChange: (value: boolean) => setSidebarIsOpen(value),
+	});
+
+	// Применить изменения
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setArticleState({
+			...articleState,
+			fontFamilyOption: fontFamily,
+			fontSizeOption: fontSize,
+			fontColor: fontColor,
+			backgroundColor: backgroundColor,
+			contentWidth: contentWidth,
+		});
+	};
+
+	// Сбросить 
+
+	const handleReset = () => {
+		setArticleState(defaultArticleState);
+		setFontFamily(defaultArticleState.fontFamilyOption);
+		setFontSize(defaultArticleState.fontSizeOption);
+		setFontColor(defaultArticleState.fontColor);
+		setBackgroundColor(defaultArticleState.backgroundColor);
+		setContentWidth(defaultArticleState.contentWidth);
+	};
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div ref={sidebarRef}>
+			<ArrowButton
+				isOpen={isSidebarOpen}
+				onClick={() => setSidebarIsOpen(!isSidebarOpen)}
+			/>
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isSidebarOpen,
+				})}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<Text size={31} weight={800} uppercase>
+						Задайте параметры
+					</	Text>
+
+					<Select
+						title='шрифт'
+						selected={fontFamily}
+						options={fontFamilyOptions}
+						onChange={setFontFamily}
+					/>
+
+					<RadioGroup
+						title='размер шрифта'
+						selected={fontSize}
+						options={fontSizeOptions}
+						name='fontSize'
+						onChange={setFontSize}
+					/>
+
+					<Select
+						title='цвет шрифта'
+						selected={fontColor}
+						options={fontColors}
+						onChange={setFontColor}
+					/>
+
+					<Separator />
+
+					<Select
+						title='цвет фона'
+						selected={backgroundColor}
+						options={backgroundColors}
+						onChange={setBackgroundColor}
+					/>
+
+					<Select
+						title='ширина контента'
+						selected={contentWidth}
+						options={contentWidthArr}
+						onChange={setContentWidth}
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
